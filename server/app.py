a71bbd077b6804a21c1cccbc4aa7bd97f5ccfe50
julius mutinda <juliusmwendwa082@gmail.com>
2026-01-14 10:08:41 +0300

    Update files

--- server/app.py
+++ server/app.py
@@ -4,5 +4,42 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+
+@app.route('/print/<string:text>')
+def print_string(text):
+    print(text)
+    return text
+
+
+@app.route('/count/<int:count>')
+def count(count):
+    output = '\n'.join(str(i) for i in range(count))
+    if output:
+        output = output + '\n'
+    return output
+
+
+@app.route('/math/<int:num1>/<operation>/<int:num2>')
+def math(num1, operation, num2):
+    if operation == '+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2
+    elif operation == '*':
+        result = num1 * num2
+    elif operation == 'div':
+        result = num1 / num2
+    elif operation == '%':
+        result = num1 % num2
+    else:
+        result = 'Unknown operation'
+    return str(result)
+
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
